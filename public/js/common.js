d42802cff7de736e266dc998ba9c70fcd2e8d284
Antônio Vinícius <vinyanalista@gmail.com>
2015-07-13 22:25:19 -0300

    Google AdSense

--- public/js/common.js
+++ public/js/common.js
@@ -5,3 +5,10 @@ var addthis_config = {
     "data_track_addressbar":true,
     "ui_language": "pt"
 };
+
+/* Google AdSense */
+
+adsbygoogle = window.adsbygoogle || [];
+adsbygoogle.push({});
+adsbygoogle.push({});
+adsbygoogle.push({});
